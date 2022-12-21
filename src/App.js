2956558f8a658bef45998fcf5e4926f213f1a070
pankaj <45347450+UchihaPan@users.noreply.github.com>
2022-12-21 16:50:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import logo from './logo.svg';
+import React, { useState } from "react";
+
+import './App.css';
+const emojiDictionary = {
+  "😊": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑":
+    "annoyance" /** add some more to show how the app now expands when there's new data */
+};
+
+/**
+ * Bonus feature
+ * converting an object to array of keys
+ */
+const emojis = Object.keys(emojiDictionary);
+function App() {
+  const [emoji, setEmoji] = useState(""); /** concept 2 is useState */
+  const [meaning, setMeaning] = useState("translation will appear here..");
+
+  function changeHandler(event) {
+    const inputEmoji = event.target.value;
+    setEmoji(inputEmoji);
+
+    if (inputEmoji in emojiDictionary) {
+      setMeaning(emojiDictionary[inputEmoji]);
+    } else {
+      setMeaning("failure to recognise this emoji");
+    }
+  }
+
+  function emojiClickHandler(inputEmoji) {
+    setMeaning(emojiDictionary[inputEmoji]);
+  }
+  return (
+<div className="App">
+      <h1>inside out</h1>
+      <input
+        onChange={changeHandler}
+        value={emoji}
+        placeholder={"Search your emoji"}
+        style={{
+          padding: "1em",
+          minWidth: "80%"
+        }}
+      />
+      <h2> {emoji} </h2> {/** Concept 1: JSX */}
+      <h3> {meaning} </h3> {/** how much part is re-rendered. */}
+      {
+        /** Bonus feature; if time permmits */
+        /**
+         * concepts to cover: mapping a list
+         * click handler on list item
+         */
+        emojis.map((emoji) => (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+          >
+            {" "}
+            {emoji}{" "}
+          </span>
+        ))
+      }
+    </div>
+  );
+}
+
+export default App;
